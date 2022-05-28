edec71ef9cbaa2da2918ade3162656bf2bfcf665
Albert Goncalves <goncalves.albert@gmail.com>
2022-05-28 11:40:06 -0400

    no-op

--- src/hello.asm
+++ src/hello.asm
@@ -14,27 +14,31 @@ format ELF64 executable 3
 ;   eax   ; syscall_number
 ;   syscall
 
-string  equ string0
-len     equ (len0 + len1)
+STRING  equ STRING0
+LEN     equ (LEN0 + LEN1)
+
+STDOUT    equ 1
+SYS_WRITE equ 1
+SYS_EXIT  equ 60
 
 segment readable executable
 
 entry $
-    mov     edx,    len         ; CPU zero extends 32-bit operation to 64-bit,
+    mov     edx,    LEN         ; CPU zero extends 32-bit operation to 64-bit,
                                 ; so we can use less bytes than `mov rdx ...`
-    lea     rsi,    [string]
-    mov     edi,    1           ; STDOUT
-    mov     eax,    1           ; sys_write
+    lea     rsi,    [STRING]
+    mov     edi,    STDOUT
+    mov     eax,    SYS_WRITE
     syscall
 
     xor     edi,    edi         ; exit code 0
-    mov     eax,    60          ; sys_exit
+    mov     eax,    SYS_EXIT
     syscall
 
 segment readable writeable
 
-string0 db  "Hello, 64-bit"
-len0    =   $ - string0
+STRING0 db  "Hello, 64-bit"
+LEN0    =   $ - STRING0
 
-string1 db  " world!", 0xA
-len1    =   $ - string1
+STRING1 db  " world!", 0xA
+LEN1    =   $ - STRING1
