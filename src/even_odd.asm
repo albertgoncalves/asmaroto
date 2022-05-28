a7366eb844d57b6e240b224f9cafcdedbe1711c8
Albert Goncalves <goncalves.albert@gmail.com>
2022-05-28 13:35:01 -0400

    develop example

--- src/even_odd.asm
+++ src/even_odd.asm
@@ -2,10 +2,75 @@ format ELF64
 
 public main
 
-extrn getpid
 extrn printf
 
-N equ 456789
+BUFFER_CAP equ 1024
+
+section '.text' executable
+
+macro PROLOGUE {
+        push    rbp     ; ==    sub     rsp,    8
+                        ;       mov     [rsp],  rbp
+        mov     rbp,    rsp
+        sub     rsp,    8           ; 16-byte alignment
+}
+
+macro EPILOGUE {
+        add     rsp,    8
+        mov     rsp,    rbp
+
+        pop     rbp     ; ==    mov     rbp,    [rsp]
+                        ;       add     rsp,    8
+        ret
+}
+
+main:
+        PROLOGUE
+
+        sub     rsp,            16
+        mov     qword [rsp],    456789
+
+        mov     rdi,            [rsp]
+        call    is_even
+
+        cmp     rax,            1
+        je      main_if_eq_1
+
+        mov     rdx,            STR_ODD
+        jmp     main_if_end
+    main_if_eq_1:
+        mov     rdx,            STR_EVEN
+
+    main_if_end:
+        mov     rdi,            STR_FMT
+        mov     rsi,            [rsp]
+        xor     rax,            rax
+        call    printf
+
+        add     rsp,            16
+        EPILOGUE
+
+is_even:
+        PROLOGUE
+    is_even_body:
+        cmp     rdi,        0
+        je      is_even_ret
+        sub     rdi,        1
+        jmp     is_odd_body
+    is_even_ret:
+        mov     rax,        1
+        EPILOGUE
+
+is_odd:
+        PROLOGUE
+    is_odd_body:
+        cmp     rdi,        0
+        je      is_odd_ret
+        sub     rdi,        1
+        jmp     is_even_body
+    is_odd_ret:
+        mov     rax,        0
+        EPILOGUE
 
 section '.data' writeable
 
@@ -13,49 +78,6 @@ STR_FMT  db "%d is %s", 0xA, 0
 STR_EVEN db "even", 0
 STR_ODD  db "odd", 0
 
-section '.text' executable
+section '.bss' writeable
 
-main:
-        push    rbp
-
-        mov     rdi,    N
-        call    is_even
-
-        cmp     rax,    0
-        je      eq_0
-        mov     rdx,    STR_EVEN
-        jmp     ne_0
-    eq_0:
-        mov     rdx,    STR_ODD
-    ne_0:
-        mov     rdi,    STR_FMT
-        mov     rsi,    N
-        xor     rax,    rax
-        call    printf
-
-        pop     rbp
-        ret
-
-is_even:
-        push    rbp
-    is_even_tco:
-        cmp     rdi,    0
-        je      is_even_zero
-        sub     rdi,    1
-        jmp     is_odd_tco
-    is_even_zero:
-        mov     rax,    1
-        pop     rbp
-        ret
-
-is_odd:
-        push    rbp
-    is_odd_tco:
-        cmp     rdi,    0
-        je      is_odd_zero
-        sub     rdi,    1
-        jmp     is_even_tco
-    is_odd_zero:
-        mov     rax,    0
-        pop     rbp
-        ret
+BUFFER rb BUFFER_CAP
