2e4d321f399d6138c0ffbaf7a78bc06946d57ebc
Albert Goncalves <goncalves.albert@gmail.com>
2022-05-30 13:40:30 -0400

    simple stack machine

---
+++ src/stack_machine.asm
@@ -0,0 +1,116 @@
+format ELF64
+
+public main
+
+extrn printf
+
+SYS_EXIT equ 60
+
+OK    equ 0
+ERROR equ 1
+
+macro STACK_SYSCALL2 {
+        pop     rdi
+        pop     rax
+        syscall
+}
+
+macro STACK_PRINTLN_I64 {
+        pop     rsi
+        mov     rdi, FORMAT_I64
+        mov     rax, 0
+        call    printf
+}
+
+macro STACK_COPY offset {
+        mov     rax, [rsp + (offset * 8)]
+        push    rax
+}
+
+macro STACK_DROP count {
+        add     rsp, 8 * count
+}
+
+macro STACK_ADD {
+        pop     rdx
+        pop     rax
+        add     rax, rdx
+        push    rax
+}
+
+macro STACK_SUB {
+        pop     rdx
+        pop     rax
+        sub     rax, rdx
+        push    rax
+}
+
+macro STACK_IDIV {
+        pop     rcx
+        pop     rax
+        cqo
+        idiv    rcx
+        push    rax
+}
+
+macro STACK_IMOD {
+        pop     rcx
+        pop     rax
+        mov     r8, rcx
+        cqo
+        idiv    rcx
+        add     r8, rdx
+        push    r8
+}
+
+section '.text' executable
+
+main:
+        push        rbp
+        mov         rbp, rsp
+
+        push        -151        ; [-151]
+        push        0           ; [-151, 0]
+        STACK_COPY  1           ; [-151, 0, -151]
+        push        12          ; [-151, 0, -151, 12]
+        STACK_ADD               ; [-151, 0, -151+12]
+        STACK_PRINTLN_I64       ; [-151, 0]
+
+        STACK_COPY  1           ; [-151, 0, -151]
+        push        12          ; [-151, 0, -151, 12]
+        STACK_SUB               ; [-151, 0, (-151)-12]
+        STACK_PRINTLN_I64       ; [-151, 0]
+
+        push        -1
+
+        STACK_COPY  2           ; [-151, 0, -1, -151]
+        push        12          ; [-151, 0, -1, -151, 12]
+        STACK_IDIV              ; [-151, 0, -1, -151/12]
+        STACK_PRINTLN_I64       ; [-151, 0, -1]
+
+        STACK_DROP  1           ; [-151, 0]
+
+        push        -151        ; [-151, 0, -151]
+        push        12          ; [-151, 0, -151, 12]
+        STACK_IMOD              ; [-151, 0, -151%12]
+
+        STACK_COPY  0           ; [-151, 0, -151%12, -151%12]
+        STACK_PRINTLN_I64       ; [-151, 0, -151%12]
+
+        STACK_DROP  3           ; []
+
+        push        rbp         ; [rbp]
+        push        rsp         ; [rbp, rsp]
+        STACK_SUB               ; [rbp-rsp]
+        STACK_PRINTLN_I64       ; []
+
+        push        SYS_EXIT    ; [SYS_EXIT]
+        push        OK          ; [SYS_EXIT, OK]
+        STACK_SYSCALL2          ; []
+        ; mov     rsp, rbp
+        ; pop     rbp
+        ; ret
+
+section '.data' writeable
+
+FORMAT_I64 db "%d", 10, 0
