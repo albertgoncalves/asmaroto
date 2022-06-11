ea458fdd153fb1bd4e56057068e9f8428a92612d
Albert Goncalves <goncalves.albert@gmail.com>
2022-06-11 13:47:27 -0400

    improve c-linked-to-asm example

--- src/lib.asm
+++ src/lib.asm
@@ -9,17 +9,25 @@ extrn c_func
 
 SYS_EXIT equ 60
 
-FMT db "This value was computed in `.c`: %d", 0xA, 0
+FMT_ASM db "  [.asm]", 0xA, "This value was computed in `.c`: %d", 0xA, 0
+FMT_C   db "Hello, world!"
+LEN_C   =  $ - FMT_C
 
 _start:
-    mov     edi, -456
+    mov     edi, -456   ; x0
+    mov     esi, 450    ; x1
+    mov     edx, 6      ; x2
+    mov     ecx, 246    ; x3
+    mov     r8, FMT_C   ; char* chars
+    mov     r9d, LEN_C  ; u32   len
     call    c_func
 
-    mov     rdi, FMT
+    mov     rdi, FMT_ASM
     mov     esi, eax
-    xor     rax, rax
+    ; NOTE: See `https://stackoverflow.com/questions/6212665/why-is-eax-zeroed-before-a-call-to-printf`.
+    xor     eax, eax
     call    printf
 
-    xor     edi,    edi         ; exit code 0
-    mov     eax,    SYS_EXIT
+    xor     edi, edi         ; exit code 0
+    mov     eax, SYS_EXIT
     syscall
--- src/lib.c
+++ src/lib.c
@@ -1,11 +1,23 @@
 #include <stdint.h>
 #include <stdio.h>
 
-typedef int32_t i32;
+typedef int32_t  i32;
+typedef uint32_t u32;
 
-i32 c_func(i32);
+// NOTE: See `https://en.wikipedia.org/wiki/X86_calling_conventions#System_V_AMD64_ABI`.
 
-i32 c_func(i32 x) {
-    printf("This value was provided by `.asm`: %d\n", x);
-    return x + -123;
+//         rdi, rsi, rdx, rcx, r8,    r9
+i32 c_func(i32, i32, i32, i32, char*, u32);
+
+i32 c_func(i32 x0, i32 x1, i32 x2, i32 x3, char* chars, u32 len) {
+    printf("  [.c]\n"
+           "These values were provided by `.asm`: [ %d, %d, %d, %d ]\n"
+           "This string was provided by `.asm`: \"%.*s\"\n",
+           x0,
+           x1,
+           x2,
+           x3,
+           len,
+           chars);
+    return x0 - x1 - x2 - x3 - -123;
 }
