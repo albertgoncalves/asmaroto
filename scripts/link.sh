d19965480000473b3a41784e385470ecebf4fc5c
albertgoncalves <goncalves.albert@gmail.com>
2022-11-13 16:42:47 -0500

    newline, no-op

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -eu
+
 fasm "$WD/src/link0.asm" "$WD/bin/link0.o"
 fasm "$WD/src/link1.asm" "$WD/bin/link1.o"
 mold -run clang -o "$WD/bin/link" "$WD/bin/link1.o" "$WD/bin/link0.o"
