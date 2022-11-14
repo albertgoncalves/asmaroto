bee137f4fdf15f4b8aca99d79e836e49a0146e25
albertgoncalves <goncalves.albert@gmail.com>
2022-11-13 22:52:37 -0500

    expand virtual stack function with `malloc`

--- src/virtual_stack.asm
+++ src/virtual_stack.asm
@@ -2,6 +2,8 @@ format ELF64
 
 public main
 
+extrn free
+extrn malloc
 extrn printf
 
 OK       equ 0
@@ -9,44 +11,56 @@ ERROR    equ 1
 
 EXIT     equ 60
 
-HEAP_CAP equ 512
+HEAP_CAP equ (512 * 8)
 
 section '.rodata'
-    FMT db 0xA, "rsp - rbp  : %ld", 0xA, "stack[-4:] : [ %ld, %ld, %ld, %ld ]", 0xA, 0
+    FMT           db 0xA, "%s", 0xA
+    _FMT0         db "  rsp - rbp  : %ld", 0xA
+    _FMT1         db "  stack[-4:] : [ %ld, %ld, %ld, %ld ]", 0xA, 0
+
+    STACK_DEFAULT db "default", 0
+    STACK_BSS     db "bss", 0
+    STACK_MALLOC  db "malloc", 0
 
 section '.bss' writeable
-    HEAP_BUFFER rq HEAP_CAP
+    HEAP_BUFFER rb HEAP_CAP
 
     PREV_RSP    rq 1
     PREV_RBP    rq 1
 
 section '.text' executable
 
-macro PRINT_STACK {
+macro PRINT_STACK string {
     mov     rdi, FMT
 
-    mov     rsi, rsp
-    sub     rsi, rbp
+    mov     rsi, string
 
-    mov     rdx, [rsp + (8 * 3)]
-    mov     rcx, [rsp + (8 * 2)]
-    mov     r8, [rsp + 8]
-    mov     r9, [rsp]
+    mov     rdx, rsp
+    sub     rdx, rbp
+
+    mov     rcx, [rsp + (8 * 3)]
+    mov     r8, [rsp + (8 * 2)]
+    mov     r9, [rsp + 8]
+
+    mov     rax, [rsp]
+    push    rax
 
     xor     eax, eax
     call    printf
+
+    add     rsp, 8
 }
 
 main:
     mov     rbp, rsp
 
-    push    5
-    push    6
+    push    9
+    push    10
 
     sub     rsp, 8 * 3
 
-    mov     qword [rsp + (8 * 2)], 7
-    mov     qword [rsp + 8], 8
+    mov     qword [rsp + (8 * 2)], 11
+    mov     qword [rsp + 8], 12
 
     add     rsp, 8
 
@@ -55,7 +69,7 @@ main:
 
     ;;
 
-    lea     rsp, [HEAP_BUFFER + (HEAP_CAP * 8)]
+    lea     rsp, [HEAP_BUFFER + HEAP_CAP]
     mov     rbp, rsp
 
     sub     rsp, 16
@@ -71,17 +85,47 @@ main:
     push    3
     push    4
 
-    PRINT_STACK
+    PRINT_STACK STACK_BSS
+
+    ;;
+
+    mov     rdi, HEAP_CAP
+    call    malloc
+
+    test    rax, rax
+    jz      error
+
+    lea     rsp, [rax + HEAP_CAP]
+    mov     rbp, rsp
+
+    push    rax
+
+    push    5
+    push    6
+    push    7
+    push    8
+
+    PRINT_STACK STACK_MALLOC
+
+    add     rsp, 8 * 4
+
+    pop     rdi
+    call    free
 
     ;;
 
     mov     rsp, qword [PREV_RSP]
     mov     rbp, qword [PREV_RBP]
 
-    PRINT_STACK
+    PRINT_STACK STACK_DEFAULT
 
     ;;
 
     mov     edi, OK
+    mov     eax, EXIT
+    syscall
+
+error:
+    mov     edi, ERROR
     mov     eax, EXIT
     syscall
