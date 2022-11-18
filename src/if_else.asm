5b50a637f7b188a76bc2af9bb6aaf2ca11951b3c
albertgoncalves <goncalves.albert@gmail.com>
2022-11-18 18:51:30 -0500

    simple if-else demo

---
+++ src/if_else.asm
@@ -0,0 +1,37 @@
+format ELF64
+
+public main
+
+extrn printf
+
+section '.rodata'
+    BRANCH_IF   db "[  if  ]", 0xA, 0
+    BRANCH_ELSE db "[ else ]", 0xA, 0
+    DONE        db "Done!", 0xA, 0
+
+section '.text' executable
+    main:
+        push    rbp
+        mov     rbp, rsp
+
+        mov     rax, 123
+
+        cmp     rax, 123
+        jnz     branch_else
+    ; branch_if:
+        mov     rdi, BRANCH_IF
+        jmp     branch_end
+    branch_else:
+        mov     rdi, BRANCH_ELSE
+    branch_end:
+        xor     eax, eax
+        call    printf
+
+        mov     rdi, DONE
+        xor     eax, eax
+        call    printf
+
+        xor     eax, eax
+        mov     rsp, rbp
+        pop     rbp
+        ret
