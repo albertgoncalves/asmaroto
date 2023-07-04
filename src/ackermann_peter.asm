3af372de23e833a452c78ab688d67a8c56fbe721
albertgoncalves <goncalves.albert@gmail.com>
2023-07-04 18:49:35 -0400

    Ackermann-Peter function demo

---
+++ src/ackermann_peter.asm
@@ -0,0 +1,60 @@
+format ELF64
+
+extrn printf
+
+public main
+
+SYS_EXIT equ 60
+
+
+section '.rodata'
+    _format db "%ld", 0xA, 0
+
+
+section '.text' executable
+    main:
+        push    rbp
+        mov     rbp, rsp
+
+        mov     rdi, 3
+        mov     rsi, 10
+        call    ackermann_peter
+
+        mov     rsi, rax
+        mov     rdi, _format
+        xor     eax, eax
+        call    printf
+
+        mov     rsp, rbp
+        pop     rbp
+        ret
+
+
+    ackermann_peter:
+        test    rdi, rdi
+        jnz     _else_0
+
+        mov     rax, rsi
+        inc     rax
+
+        ret
+
+    _else_0:
+        test    rsi, rsi
+        jnz     _else_1
+
+        dec     rdi
+        mov     rsi, 1
+        jmp     ackermann_peter
+
+    _else_1:
+        push    rdi
+
+        dec     rsi
+        call    ackermann_peter
+
+        mov     rsi, rax
+        pop     rdi
+        dec     rdi
+
+        jmp     ackermann_peter
