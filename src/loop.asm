49c6be5234aa9dd17f9522f7f47a3072fa3817a6
albertgoncalves <goncalves.albert@gmail.com>
2024-09-05 09:57:50 -0400

    just a simple loop

---
+++ src/loop.asm
@@ -0,0 +1,57 @@
+format ELF64
+
+public _start
+
+extrn printf
+extrn _exit
+
+section '.rodata'
+    format_string db "%d", 10, 0
+
+section '.text' executable
+    _start:
+        call    f
+
+        xor     eax,    eax
+        call    _exit
+
+    ; function f() {
+    ;     var x = 0;
+    ;     var y = 0;
+    ;
+    ;     for (;;) {
+    ;         if (x > 10) {
+    ;             break;
+    ;         }
+    ;         y += x;
+    ;         x += 1;
+    ;     }
+    ;
+    ;     console.log(y);
+    ; }
+
+    f:
+        mov     rcx,    0   ; x
+        mov     rdx,    0   ; y
+        ; jmp     __0__
+
+    __0__:
+        cmp     rcx,    10
+        jg      __2__
+        ; jmp     __1__
+
+    __1__:
+        add     rdx,    rcx
+        ; add     rcx,    1
+        inc     rcx
+        jmp     __0__
+
+    __2__:
+        push    rdx
+        mov     rdi,    format_string
+        mov     rsi,    rdx
+        xor     eax,    eax
+        call    printf
+        pop     rdx
+
+        ret
