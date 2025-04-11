50715adfa0a966140654ef6bec18ef257d2b8f30
albertgoncalves <goncalves.albert@gmail.com>
2025-04-10 20:52:30 -0400

    implement basic lazy object demo

---
+++ src/lazy.asm
@@ -0,0 +1,89 @@
+format ELF64
+
+public _start
+
+extrn _exit
+
+extrn printf
+extrn usleep
+
+section '.rodata'
+    string_work  db "!", 10, 0
+    string_cache db ".", 10, 0
+    string_i64   db "%ld", 10, 0
+
+section '.bss' writeable
+    OBJECT rq 4
+
+section '.text' executable
+    _start:
+        push    rbp
+        mov     rbp, rsp
+
+        mov     qword [OBJECT], work
+        mov     qword [OBJECT + 8], 0
+        mov     qword [OBJECT + (8 * 2)], force
+
+        mov     rdi, OBJECT
+        mov     rax, [OBJECT + (8 * 2)]
+        call    rax
+
+        mov     rdi, OBJECT
+        mov     rax, [OBJECT + (8 * 2)]
+        call    rax
+
+        mov     rdi, OBJECT
+        mov     rax, [OBJECT + (8 * 2)]
+        call    rax
+
+        mov     rsi, rax
+        mov     rdi, string_i64
+        xor     eax, eax
+        call    printf
+
+        xor     edi, edi
+        call    _exit
+
+    work:
+        mov     rdi, string_work
+        xor     eax, eax
+        call    printf
+
+        mov     rdi, 500000
+        call    usleep
+
+        mov     rax, 123
+        ret
+
+    ; Lazy {
+    ;     func:  (void*) -> void*
+    ;     args:  void*,
+    ;     force: (Lazy*) -> void*
+    ;     value: void*,
+    ; }
+
+    force:
+        mov     r8, rdi
+        mov     rax, [r8]
+        mov     rdi, [r8 + 8]
+
+        push    r8
+        call    rax
+        pop     r8
+
+        mov     qword [r8 + (8 * 3)], rax
+        mov     qword [r8 + (8 * 2)], cache
+
+        ret
+
+    cache:
+        push    rdi
+
+        mov     rdi, string_cache
+        xor     eax, eax
+        call    printf
+
+        pop     rdi
+
+        mov     rax, [rdi + (8 * 3)]
+        ret
